4b6fc78ee27490dd44b1440400426435201bb20a
Krzysiek Szularz <szuliq@gmail.com>
2013-04-26 15:58:56 +0200

    Let the user specify IE versions for GCF tagging
    as discussed in aldryn/addons-overview#15

--- aldryn_google_chrome_frame/models.py
+++ aldryn_google_chrome_frame/models.py
@@ -2,14 +2,22 @@
 from cmscloud.template_api import registry
 from django.conf import settings
 
-META_TAG = '<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">'
 
-registry.add_to_head(META_TAG)
+def get_meta_version(max_version):
+    assert 6 <= max_version <= 9
+    if max_version == 9:
+        return '1'
+    else:
+        return 'IE%d' % (max_version, )
 
-PROMPT_SCRIPT = """<!--[if lte IE 9 ]>
+META_TAG = '<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=%(meta_version)s">'
+
+registry.add_to_head(META_TAG % {'meta_version': get_meta_version(settings.GOOGLE_CHROME_FRAME_MAX_VERSION)})
+
+PROMPT_SCRIPT = """<!--[if lte IE %(max_version)d ]>
     <script src="//ajax.googleapis.com/ajax/libs/chrome-frame/1.0.2/CFInstall.min.js"></script>
     <script>window.attachEvent("onload",function(){CFInstall.check({mode:"overlay"})})</script>
 <![endif]-->"""
 
 if getattr(settings, 'GOOGLE_CHROME_FRAME_PROMPT', False):
-    registry.add_to_tail(PROMPT_SCRIPT)
+    registry.add_to_tail(PROMPT_SCRIPT % {'max_version': settings.GOOGLE_CHROME_FRAME_MAX_VERSION})
--- cmscloud_config.py
+++ cmscloud_config.py
@@ -4,8 +4,12 @@ from cmscloud_client import forms
 
 class Form(forms.BaseForm):
 
-    installation_prompt = forms.CheckboxField('Display Google Chrome Frame installation prompt')
+    CHOICES = [(x, 'IE%d' % (x,)) for x in range(9, 5, -1)]
+
+    max_version = forms.SelectFields(u'Max IE version for Chrome rendering', choices=CHOICES)
+    installation_prompt = forms.CheckboxField(u'Display installation prompt')
 
     def to_settings(self, data, settings):
-        settings['GOOGLE_CHROME_FRAME_PROMPT'] = data['installation_prompt']
+        settings['GOOGLE_CHROME_FRAME_PROMPT'] = data['max_version']
+        settings['GOOGLE_CHROME_FRAME_MAX_VERSION'] = data['installation_prompt']
         return settings
