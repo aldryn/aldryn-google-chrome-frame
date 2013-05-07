33140bcd9dfdd374e45e61132fe1c4e68ca2ca7d
Krzysiek Szularz <szuliq@gmail.com>
2013-05-07 08:34:27 +0200

    Don't make the installation prompt required

--- cmscloud_config.py
+++ cmscloud_config.py
@@ -7,7 +7,7 @@ class Form(forms.BaseForm):
     CHOICES = [(x, 'IE%d' % (x,)) for x in range(9, 5, -1)]
 
     max_version = forms.SelectField(u'Max IE version for Chrome rendering', choices=CHOICES)
-    installation_prompt = forms.CheckboxField(u'Display installation prompt')
+    installation_prompt = forms.CheckboxField(u'Display installation prompt', required=False)
 
     def to_settings(self, data, settings):
         settings['GOOGLE_CHROME_FRAME_PROMPT'] = data['max_version']
